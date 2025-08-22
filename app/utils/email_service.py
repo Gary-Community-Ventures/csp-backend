b28788d9e70fc40cc6973ea2eb6bcce411cd2ca1
CalebPena <62856626+CalebPena@users.noreply.github.com>
2025-08-22 13:48:35 -0600

    fix parameter bug (#76)

--- app/utils/email_service.py
+++ app/utils/email_service.py
@@ -440,13 +440,12 @@ def send_family_invite_accept_email(
     provider_id: str,
     parent_name: str,
     parent_id: str,
-    child_name: str,
-    child_id: str,
+    children: list[KeyMap],
 ):
     from_email, to_emails = get_internal_emails()
 
     current_app.logger.info(
-        f"Sending accept invite request email to {to_emails} for provider ID: {provider_id} for family ID: {parent_id} for child ID: {child_id}"
+        f"Sending accept invite request email to {to_emails} for provider ID: {provider_id} for family ID: {parent_id} for child IDs: {[c.get(ChildColumnNames.ID) for c in children]}"
     )
 
     rows = [
@@ -458,11 +457,15 @@ def send_family_invite_accept_email(
             title=f"Parent Name",
             value=f"{parent_name} (ID: {parent_id})",
         ),
-        SystemMessageRow(
-            title="Child Name",
-            value=f"{child_name} (ID: {child_id})",
-        ),
     ]
+
+    for child in children:
+        rows.append(
+            SystemMessageRow(
+                title="Child Name",
+                value=f"{format_name(child)} (ID: {child.get(ChildColumnNames.ID)})",
+            )
+        )
 
     subject = "New Add Family Invite Accepted Notification"
     description = f"A new family invite request has been submitted:"
