adc87f5485df722b229a90992a20e232a9bfdda8
tlillis <thomasglillis@gmail.com>
2025-09-17 15:16:40 -0600

    add cpr certified to fe

--- app/supabase/tables.py
+++ app/supabase/tables.py
@@ -119,6 +119,7 @@ class Provider(Table):
     STATE = Column("care_location_state")
     ZIP = Column("care_location_zip")
     LANGUAGE = Column("language", enum_column(Language))
+    CPR_CERTIFIED = Column("cpr_certified")
     CPR_TRAINING_LINK = Column("cpr_training_link")
     CPR_ONLINE_TRAINING_COMPLETED_AT = Column("cpr_online_training_completed_at", datetime_column)
     CHILD_SAFETY_MODULE_TRAINING_COMPLETED_AT = Column("child_safety_module_training_completed_at", datetime_column)
