0b24141b7b88a63ce410d163f76427dc8b817795
Caleb <caleblpena@gmail.com>
2025-09-26 16:39:43 -0600

    merge migrations fr this time

---
+++ migrations/versions/05706026adc8_merge_attendance_days_and_email_record_.py
@@ -0,0 +1,24 @@
+"""Merge attendance_days and email_record branches
+
+Revision ID: 05706026adc8
+Revises: d458ce0f02ff, e7f96bcb63e2
+Create Date: 2025-09-26 22:38:34.208833
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '05706026adc8'
+down_revision = ('d458ce0f02ff', 'e7f96bcb63e2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
