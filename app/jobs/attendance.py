c065024ea3202406e2e7b1f1b13d25600504851f
tlillis <thomasglillis@gmail.com>
2025-10-30 12:36:44 -0600

    attendance

--- app/jobs/attendance.py
+++ app/jobs/attendance.py
@@ -9,10 +9,10 @@ from . import job_manager
 @job_manager.job
 def create_attendance_job(**kwargs):
     create_attendance()
-    send_attendance_emails(True, True)
 
 
 def schedule_attendance_job():
+    # Run at 8:00 AM UTC on Mondays (1:00 AM MST / 2:00 AM MDT)
     cron_schedule = "0 8 * * 1"
 
     current_app.logger.info(f"Scheduling attendance job with cron '{cron_schedule}'")
@@ -21,18 +21,29 @@ def schedule_attendance_job():
 
 
 @job_manager.job
-def create_reminder_job(**kwargs):
+def send_attendance_communications_job(**kwargs):
     send_attendance_emails(True, True)
 
 
+def schedule_attendance_initial_communications_job(**kwargs):
+    # Run at 3:00 PM UTC on Mondays (8:00 AM MST / 9:00 AM MDT)
+    cron_schedule = "0 15 * * 1"
+
+    current_app.logger.info(f"Scheduling attendance reminder job with cron '{cron_schedule}'")
+
+    return send_attendance_communications_job.schedule_cron(cron_schedule)
+
+
 def schedule_reminder_job():
-    cron_schedule_first = "0 8 * * 5"
-    cron_schedule_second = "0 8 * * 0"
+    # Run at 3:00 PM UTC on Fridays (8:00 AM MST / 9:00 AM MDT)
+    cron_schedule_first = "0 15 * * 5"
+    # Run at 3:00 PM UTC on Sundays (8:00 AM MST / 9:00 AM MDT)
+    cron_schedule_second = "0 15 * * 0"
 
     current_app.logger.info(
         f"Scheduling attendance reminder job with cron '{cron_schedule_first}' and '{cron_schedule_second}'"
     )
 
-    return create_reminder_job.schedule_cron(cron_schedule_first), create_reminder_job.schedule_cron(
-        cron_schedule_second
-    )
+    return send_attendance_communications_job.schedule_cron(
+        cron_schedule_first
+    ), send_attendance_communications_job.schedule_cron(cron_schedule_second)
